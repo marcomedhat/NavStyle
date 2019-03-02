562681f5b3fa9a018803c645d35905436bf483ec
Marco-Medhat <eng.marcomedhat@gmail.com>
2019-03-02 10:47:19 +0200

    Updates

---
+++ precache-manifest.9654dc617cfd8a08bffa9301191c6281.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c9c7fb15e528119f9394",
+    "url": "/NavStyle/static/css/main.04d32af7.chunk.css"
+  },
+  {
+    "revision": "c9c7fb15e528119f9394",
+    "url": "/NavStyle/static/js/main.c9c7fb15.chunk.js"
+  },
+  {
+    "revision": "2ccb031dad66f2bf5414",
+    "url": "/NavStyle/static/js/runtime~main.2ccb031d.js"
+  },
+  {
+    "revision": "f04ce7a2ce7617bb7f8c",
+    "url": "/NavStyle/static/css/2.ff499641.chunk.css"
+  },
+  {
+    "revision": "f04ce7a2ce7617bb7f8c",
+    "url": "/NavStyle/static/js/2.f04ce7a2.chunk.js"
+  },
+  {
+    "revision": "e2749b12825080a6b6af14075ad3a15a",
+    "url": "/NavStyle/index.html"
+  }
+];
